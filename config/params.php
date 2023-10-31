b9e0cb9f733d139521f62800bac5043aa65610fc
ariefan <ariefan.dipokusumo.wibowo@mail.ugm.ac.id>
2023-10-31 09:11:04 +0700

    bugfix kunci inggris dan kunci serep

--- config/params.php
+++ config/params.php
@@ -1,7 +1,9 @@
 <?php
 
 return [
-    'adminEmail' => 'admin@example.com',
-    'senderEmail' => 'noreply@example.com',
-    'senderName' => 'Example.com mailer',
+    'adminEmail'   => 'admin@example.com',
+    'senderEmail'  => 'noreply@example.com',
+    'senderName'   => 'Example.com mailer',
+    'kunciInggris' => '',
+    'kunciSerep'   => '',
 ];
